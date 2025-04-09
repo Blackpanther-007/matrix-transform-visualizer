5f3a87feb992984d3fec32f942ba17ddeeaf4f11
vedant <vedantdjadhav712@gmail.com>
2025-04-10 00:48:30 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,50 +1,50 @@
-# import numpy as np
-# import matplotlib.pyplot as plt
-# from matplotlib.animation import FuncAnimation 
-
-# x = np.linspace(-10, 10,20)
-# y = np.linspace(-10, 10,20)
-# X,Y = np.meshgrid(x,y)
-
-# U = -Y
-# V = X
-
-
-# plt.quiver(X,Y,U,V, scale=100, color='green', width=0.005)
-# plt.xlim(-10,10)
-# plt.ylim(-10,10)
-# plt.grid()
-# plt.show()
-
-#animation 
 import numpy as np
 import matplotlib.pyplot as plt
-from matplotlib.animation import FuncAnimation 
+from matplotlib.animation import FuncAnimation
 
+# Physics setup
 g = 9.8
-angle = np.pi/4
+angle = np.pi/4  # 45 degrees
 vo = 40
-t_max = 2 * vo * np.sin(angle) / g
+t_max = 2 * vo * np.sin(angle) / g  # Time until projectile hits ground
+t_values = np.linspace(0, t_max, 100)  # Time steps
 
+# Trajectory calculations
+x_values = vo * np.cos(angle) * t_values
+y_values = vo * np.sin(angle) * t_values - 0.5 * g * t_values**2
+
+# Plot setup
 fig, ax = plt.subplots()
-line, = ax.plot([], [], 'ro')  # This plots a red dot initially
-ax.set_xlim(0, 100)
-ax.set_ylim(0, 100)
+line, = ax.plot([], [], 'ro')  # Red dot for current position
+trail, = ax.plot([], [], 'b-', alpha=0.5)  # Blue line for trajectory
+ax.set_xlim(0, max(x_values) + 10)
+ax.set_ylim(0, max(y_values) + 10)
+ax.grid()
 
-# Initialize the plot
+# Initialize animation
 def init():
     line.set_data([], [])
-    return line,
+    trail.set_data([], [])
+    return line, trail
 
-# Update the plot for each frame
-def update(t):
-    x = vo * np.cos(angle) * t
-    y = vo * np.sin(angle) * t - 0.5 * g * t**2
-    line.set_data(x, y)  # Update position of the single point
-    return line,
+# Update animation frame
+def update(i):
+    line.set_data([x_values[i]], [y_values[i]])  # Wrap in lists!
+    trail.set_data(x_values[:i+1], y_values[:i+1])
+    return line, trail
 
-# Create the animation
-ani = FuncAnimation(fig, func=update, frames=np.linspace(0, t_max, 100), init_func=init, blit=True, interval=20)
+# Create animation
+ani = FuncAnimation(
+    fig, 
+    update, 
+    frames=len(t_values), 
+    init_func=init, 
+    blit=True, 
+    interval=20, 
+    repeat=False
+)
 
-plt.grid()
+plt.title("Projectile Motion Animation")
+plt.xlabel("Horizontal Distance (m)")
+plt.ylabel("Vertical Distance (m)")
 plt.show()
