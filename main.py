0e58bbb14b4e6d4cc33d8328e68197a265f5ed25
Blackpanther-007 <vedantdjadhav712@gmail.com>
2025-04-10 16:58:30 +0530

    added ploting of 2-D vector

--- main.py
+++ main.py
@@ -2,4 +2,16 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
 
-x = 2
+origin = np.array([0,0])
+v1 = np.array([3,1])
+v2 = np.array([-4, 6])
+
+fig, ax = plt.subplots()
+ax.quiver(*origin, *v1, color='b', scale=1, scale_units='xy', angles='xy', label= 'Vector 1 (3, 1)')
+ax.quiver(*origin, *v2, color='r', scale=1, scale_units='xy', angles='xy', label= 'Vector 2 (-4, 6)')
+ax.set_xlim(-10, 10)
+ax.set_ylim(-10, 10)
+ax.grid()
+ax.legend()
+ax.set_aspect('equal')
+plt.show()
